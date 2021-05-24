8d2f9f5e6eaf7553c814b993e32a74833b0cfff0
TiscotSerVices <tiscot2019@gmail.com>
2021-05-24 16:16:42 +0200

    removed versionbar

--- src/components/VersionBar/index.tsx
+++ src/components/VersionBar/index.tsx
@@ -17,36 +17,3 @@ const Wrapper = styled.div`
     padding-left: 240px;
   }
 `
-
-const VersionBar = () => {
-  return (
-    <Wrapper>
-      <Text bold mr="16px">
-        Version:
-      </Text>
-      <ButtonMenu variant="primary" scale="sm" activeIndex={1}>
-        <ButtonMenuItem as="a" href="https://exchange.pancakeswap.finance/">
-          V2
-        </ButtonMenuItem>
-        <ButtonMenuItem as="a" href="https://v1exchange.pancakeswap.finance/#/">
-          V1 (old)
-        </ButtonMenuItem>
-        <ButtonMenuItem as="a" href="https://hiccup.pancakeswap.finance/#/">
-          V2 Discontinued
-        </ButtonMenuItem>
-      </ButtonMenu>
-      <Button
-        variant="subtle"
-        as={Link}
-        href="https://v1exchange.pancakeswap.finance/#/migrate"
-        endIcon={<HelpIcon color="white" />}
-        scale="sm"
-        ml="16px"
-      >
-        Help
-      </Button>
-    </Wrapper>
-  )
-}
-
-export default VersionBar
