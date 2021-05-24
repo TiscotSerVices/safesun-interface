7a7e09603ae10ce12c0595d2c764fdb90277bf70
TiscotSerVices <tiscot2019@gmail.com>
2021-05-24 16:41:03 +0200

    removed language settings

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -38,65 +38,8 @@ const BodyWrapper = styled.div`
   }
 `
 
-const CACHE_KEY = 'pancakeSwapLanguage'
-
-export default function App() {
-  const [selectedLanguage, setSelectedLanguage] = useState<any>(undefined)
-  const [translatedLanguage, setTranslatedLanguage] = useState<any>(undefined)
-  const [translations, setTranslations] = useState<Array<any>>([])
-  const apiKey = `${process.env.REACT_APP_CROWDIN_APIKEY}`
-  const projectId = parseInt(`${process.env.REACT_APP_CROWDIN_PROJECTID}`)
-  const fileId = 6
-
   const credentials: Credentials = {
     token: apiKey,
-  }
-
-  const stringTranslationsApi = new StringTranslations(credentials)
-
-  const getStoredLang = (storedLangCode: string) => {
-    return allLanguages.filter((language) => {
-      return language.code === storedLangCode
-    })[0]
-  }
-
-  useEffect(() => {
-    const storedLangCode = localStorage.getItem(CACHE_KEY)
-    if (storedLangCode) {
-      const storedLang = getStoredLang(storedLangCode)
-      setSelectedLanguage(storedLang)
-    } else {
-      setSelectedLanguage(EN)
-    }
-  }, [])
-
-  const fetchTranslationsForSelectedLanguage = async () => {
-    stringTranslationsApi
-      .listLanguageTranslations(projectId, selectedLanguage.code, undefined, fileId, 200)
-      .then((translationApiResponse) => {
-        if (translationApiResponse.data.length < 1) {
-          setTranslations(['error'])
-        } else {
-          setTranslations(translationApiResponse.data)
-        }
-      })
-      .then(() => setTranslatedLanguage(selectedLanguage))
-      .catch((error) => {
-        setTranslations(['error'])
-        console.error(error)
-      })
-  }
-
-  useEffect(() => {
-    if (selectedLanguage) {
-      fetchTranslationsForSelectedLanguage()
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedLanguage])
-
-  const handleLanguageSelect = (langObject: LangType) => {
-    setSelectedLanguage(langObject)
-    localStorage.setItem(CACHE_KEY, langObject.code)
   }
 
   useGetDocumentTitlePrice()
@@ -105,15 +48,6 @@ export default function App() {
     <Suspense fallback={null}>
       <HashRouter>
         <AppWrapper>
-          <LanguageContext.Provider
-            value={{
-              selectedLanguage,
-              setSelectedLanguage: handleLanguageSelect,
-              translatedLanguage,
-              setTranslatedLanguage,
-            }}
-          >
-            <TranslationsContext.Provider value={{ translations, setTranslations }}>
               <Menu>
                 <BodyWrapper>
                   <Popups />
@@ -136,8 +70,6 @@ export default function App() {
                   </Web3ReactManager>
                 </BodyWrapper>
               </Menu>
-            </TranslationsContext.Provider>
-          </LanguageContext.Provider>
         </AppWrapper>
       </HashRouter>
     </Suspense>
