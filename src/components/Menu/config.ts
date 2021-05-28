c614a5d363a3993d2add203e21b4f10318c7b96a
TiscotSerVices <tiscot2019@gmail.com>
2021-05-28 19:05:40 +0200

    Cleared menu page

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -2,36 +2,5 @@ import { MenuEntry } from '@pancakeswap-libs/uikit'
 
 const config: MenuEntry[] = [
 
-  {
-    label: 'Trade',
-    icon: 'TradeIcon',
-    initialOpenState: true,
-    items: [
-      {
-        label: 'LP Migration',
-        href: '/migrate',
-      },
-      {
-        label: 'Exchange',
-        href: 'https://exchange.pancakeswap.finance/#/swap',
-      },
-      {
-        label: 'Liquidity',
-        href: 'https://exchange.pancakeswap.finance/#/pool',
-      },
-      {
-        label: 'V1 Liquidity (Old)',
-        href: '/pool',
-      },
-    ],
-  },
-  {
-    label: 'Info',
-    icon: 'InfoIcon',
-    href: 'https://bscscan.com',
-  },
-
-
-]
 
 export default config
